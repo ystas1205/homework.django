4ea67cd03d9b442e15343ce72924f03c4af19411
ystas <ystas1205@gmail.com>
2023-12-21 14:58:06 +0300

    Add ci

--- logistic/urls.py
+++ logistic/urls.py
@@ -5,5 +5,4 @@ from logistic.views import ProductViewSet, StockViewSet
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
-
 urlpatterns = router.urls
