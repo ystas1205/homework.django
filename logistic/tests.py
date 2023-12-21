24cd522077a475ffd794d5d8c49fd92608ed8341
ystas <ystas1205@gmail.com>
2023-12-21 18:49:29 +0300

    Fix

--- logistic/tests.py
+++ logistic/tests.py
@@ -5,5 +5,5 @@ from rest_framework.test import APIClient
 class TestSomething(TestCase):
     def test_ok(self):
         client = APIClient()
-        response = client.get('/api/v1/')
+        response = client.get('/api/v1/test/')
         assert response.status_code == 200
--- stocks_products/urls.py
+++ stocks_products/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v2/', include('logistic.urls')),
+    path('api/v1/', include('logistic.urls')),
 
 ]
